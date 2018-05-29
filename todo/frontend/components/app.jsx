00ec8134cbbd99ca498a4f7184c8317c48bcd6d8
Shawn Salat <Shawn.Salat@gmail.com>
2018-05-28 17:55:08 -0700

    create  todo selectors

---
+++ todo/frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>App </h1>
+    <h2> hi! </h2>
+    <h3> 💀 </h3>
+  </div>
+)
+
+export default App;
--- todo/frontend/todo_redux.jsx
+++ todo/frontend/todo_redux.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root'
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -8,6 +10,5 @@ document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   window.store = store;
 
-  //<Root store={store} />
-  ReactDOM.render(<h1>Todos App</h1>, rootEl)
+  ReactDOM.render(<Root store={store} />, rootEl)
 })

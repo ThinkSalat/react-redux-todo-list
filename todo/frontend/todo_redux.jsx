98ed82ee96763f59673be81eda273dafce66753d
andzu schaefer <countermatter7@gmail.com>
2018-05-29 11:59:03 -0700

    Complete Phase 3 Selectors

--- todo/frontend/todo_redux.jsx
+++ todo/frontend/todo_redux.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root'
-
+import {allTodos} from './reducers/selectors'
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
   const store = configureStore();
   window.store = store;
-
+  window.allTodos = allTodos;
   ReactDOM.render(<Root store={store} />, rootEl)
 })
